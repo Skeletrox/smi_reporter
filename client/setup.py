a7a8acbf37e3b641e828cd9a00463c64637bf85b
Skeletrox <android.svr@gmail.com>
2020-01-14 23:50:43 -0800

    update IP and port checking

--- client/setup.py
+++ client/setup.py
@@ -7,7 +7,34 @@ from uuid import uuid4
 # Setup for the NVIDIA-SMI reporter. This should be run first!
 # 
 #
+
 #
+# Helper method to parse IP address
+#
+
+def isIP(inpstr):
+    if inpstr == 'localhost':
+        return True
+
+    cache = ''
+    numdots = 0
+    for i in inpstr:    
+        if i == '.':
+            cache = ''
+            numdots += 1
+            if numdots == 4:
+                return False
+        else:
+            cache += i
+            try:
+                x = int(cache)
+                if x > 255:
+                    raise ValueError
+            except Exception as e:
+                return False
+
+    return numdots == 3
+
 
 #
 # Check for the config file. If it exists, terminate
@@ -32,14 +59,14 @@ port = input("[+] Enter the port the server listens on (default: 13768): ")
 
 # Validate data. TODO: Check for host IP?
 
-if len(host) < 1:
+if not isIP(host):
+    print("[x] Invalid host, defaulting to localhost")
     host = "localhost"
-
-if len(port) < 1:
-    port = "13768"
 
 try:
     port = int(port)
+    if port > 65536 or port < 1:
+        raise ValueError
 except Exception:
     print("[x] Invalid port number, defaulting to 13768")
     port = 13768
--- server/setup.py
+++ server/setup.py
@@ -3,10 +3,34 @@ import sys
 
 #
 #
+#
 # Setup for the NVIDIA-SMI reporter. This should be run first!
 # 
 #
 #
+
+def isIP(inpstr):
+    if inpstr == 'localhost':
+        return True
+
+    cache = ''
+    numdots = 0
+    for i in inpstr:    
+        if i == '.':
+            cache = ''
+            numdots += 1
+            if numdots == 4:
+                return False
+        else:
+            cache += i
+            try:
+                x = int(cache)
+                if x > 255:
+                    raise ValueError
+            except Exception as e:
+                return False
+
+    return numdots == 3
 
 #
 # Check for the config file. If it exists, terminate
@@ -30,31 +54,33 @@ database = input("[+] Enter the name of the database (default: nvidia_smi): ")
 
 serverPort = input("[+] Enter the port this machine listens on (default: 13768): ")
 
-# Validate data. TODO: Check for host IP?
+# Validate data.
 
-if len(host) < 1:
+if not isIP(host):
+    print("[x] Invalid host, defaulting to localhost")
     host = "localhost"
-
-if len(port) < 1:
-    port = "8086"
-
-if len(serverPort) < 1:
-    serverPort = "13768"
 
 
 try:
     port = int(port)
+    if port > 65536 or port < 1:
+        raise ValueError
 except Exception:
     print("[x] Invalid port number, defaulting to 8086")
     port = 8086
 
+
 try:
-    serverPort = int(serverPort)
+    serverPort = int(port)
+    if serverPort > 65536 or serverPort < 1:
+        raise ValueError
 except Exception:
-    print("[x] Invalid port number, defaulting to 13768")
+    print("[x] Invalid server port number, defaulting to 13768")
     serverPort = 13768
 
+
 if len(database) < 1:
+    print("[x] Invalid database name, defaulting to nvidia_smi")
     database = "nvidia_smi"
 
 
